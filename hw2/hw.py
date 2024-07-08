5b4bd587c3287d8b8a630a2d7ddf6974fcfc8e5b
saba shelia <159024845+MrSheli@users.noreply.github.com>
2024-07-08 23:18:18 +0400

    4

---
+++ hw2/hw.py
@@ -0,0 +1,12 @@
+#შექმენით ფუნქცია, რომელსაც გადასცემთ მომხმარებლისგან
+#მიღებულ სახელსა და გვარს. შემდეგ ისინი დაამატეთ სიაში და დააბრუნეთ სია.
+
+
+def user(name,lastname):
+  lst = []
+
+  lst.append(str(name))
+  lst.append(str(lastname))
+  print(lst)
+
+user("saba", "shelia")
---
+++ hw6/hw.py
@@ -0,0 +1,45 @@
+# შექმენით ფუნქცია, რომელსაც გადაეცემა რიცხვების სია. 
+# თქვენ უნდა დააბრუნოთ ამ სიის საშუალო არითმეტიკული ( ჯამი / სიგრძე )
+
+
+
+def dup(lst):
+  lst3 = set(lst)
+  print(list(lst3))
+
+lstt = [1,2,3,4,5,2,2,2]
+dup(lstt)
+    for char in "row":
+        if len(row) % 2 != 0:
+            lst.append(row[-1])
+            row.replace(row[-1],"")
+        else:
+            if char == "R":
+                if row[row.index(char) + 1] == "G":
+                    lst.append("B")
+                elif char == "R":
+                    lst.append
+                elif row[row.index(char) + 1] == "B":
+                    lst.append("G")
+            elif char == "G":
+                if row[row.index(char) + 1] == "G":
+                    lst.append("G")
+                elif row[row.index(char) + 1] == "R":
+                    lst.append("B")
+
+                elif row[row.index(char) + 1] == "B":
+                    lst.append("R")
+            elif char == "B":
+                if row[row.index(char) + 1] == "G":
+                    lst.append("R")
+                elif row[row.index(char) + 1] == "R":
+                    lst.append("G")
+
+                elif row[row.index(char) + 1] == "B":
+                    lst.append("B")          
+    m = set(lst)
+    return list(m)
+        
+
+i = [] 
+print(len(i))
